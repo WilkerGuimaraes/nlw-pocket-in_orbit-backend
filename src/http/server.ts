61227088d25f64f60fa6a2f124460624bb0b4bbd
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2025-07-13 17:36:34 -0300

    feat: add get-week-sumary function and route

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -16,5 +17,6 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP server running!'))
