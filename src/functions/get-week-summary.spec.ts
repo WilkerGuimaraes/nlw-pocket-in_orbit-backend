7adc9e049d0a58f7e49e5b6733506887b8334cef
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2025-07-17 16:18:09 -0300

    test: add unit tests to get-week summary function

---
+++ src/functions/get-week-summary.spec.ts
@@ -0,0 +1,76 @@
+import dayjs from 'dayjs'
+import { describe, expect, it } from 'vitest'
+import { makeGoal } from '../../tests/factories/make-goal'
+import { makeGoalCompletion } from '../../tests/factories/make-goal-completion'
+import { makeUser } from '../../tests/factories/make-user'
+import { getWeekSummary } from './get-week-summary'
+
+describe('get week week summary', () => {
+  it('should be able to get week week summary', async () => {
+    const user = await makeUser()
+
+    const weekStartsAt = dayjs(new Date(2025, 9, 10))
+      .startOf('week')
+      .toDate()
+
+    const goal1 = await makeGoal({
+      userId: user.id,
+      title: 'Meditar',
+      desiredWeeklyFrequency: 2,
+      createdAt: weekStartsAt,
+    })
+    const goal2 = await makeGoal({
+      userId: user.id,
+      title: 'Nadar',
+      desiredWeeklyFrequency: 1,
+      createdAt: weekStartsAt,
+    })
+    const goal3 = await makeGoal({
+      userId: user.id,
+      title: 'Ler',
+      desiredWeeklyFrequency: 3,
+      createdAt: weekStartsAt,
+    })
+
+    await makeGoalCompletion({
+      goalId: goal1.id,
+      createdAt: dayjs(weekStartsAt).add(2, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal2.id,
+      createdAt: dayjs(weekStartsAt).add(2, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal3.id,
+      createdAt: dayjs(weekStartsAt).add(3, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal3.id,
+      createdAt: dayjs(weekStartsAt).add(5, 'days').toDate(),
+    })
+
+    const result = await getWeekSummary({
+      userId: user.id,
+      weekStartsAt,
+    })
+
+    expect(result).toEqual({
+      summary: {
+        completed: 4,
+        total: 6,
+        goalsPerDay: {
+          '2025-10-10': expect.arrayContaining([
+            expect.objectContaining({ title: 'Ler' }),
+          ]),
+          '2025-10-08': expect.arrayContaining([
+            expect.objectContaining({ title: 'Ler' }),
+          ]),
+          '2025-10-07': expect.arrayContaining([
+            expect.objectContaining({ title: 'Meditar' }),
+            expect.objectContaining({ title: 'Nadar' }),
+          ]),
+        },
+      },
+    })
+  })
+})
